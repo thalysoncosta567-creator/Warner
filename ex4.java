c4c4af0d12773efd3793d8440b4b4c32c6108734
Thalyson_Gama_gc <thalysoncosta567@gmail.com>
2026-03-18 15:03:05 -0300

    Add Ex4 class with main method and tri instance

---
+++ ex4.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ex4;
+
+/**
+ *
+ * @author thaly
+ */
+public class Ex4 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        tri cadastro = new tri();
+        cadastro.entrada();
+        cadastro.imprimir();
+    }
+    
+}
