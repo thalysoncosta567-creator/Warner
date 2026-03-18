454f47fc89c65bff9595dccd30b1e0f7a8cf4484
Thalyson_Gama_gc <thalysoncosta567@gmail.com>
2026-03-18 17:34:02 -0300

    Add Ex10 class with main method for Media operations

---
+++ ex9-10.java
@@ -0,0 +1,12 @@
+package ex10;
+
+public class Ex10 {
+
+    public static void main(String[] args) {
+        Media cadastro = new Media();
+
+        cadastro.entrada();
+        cadastro.calcular();
+        cadastro.imprimir();
+    }
+}
