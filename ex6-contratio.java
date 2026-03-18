5b96e5085c3183dbd4aa6330f0be6fa444dd3f68
Thalyson_Gama_gc <thalysoncosta567@gmail.com>
2026-03-18 15:35:36 -0300

    Create ex6-contratio.java

---
+++ ex6-contratio.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ex6;
+
+/**
+ *
+ * @author thaly
+ */
+public class Ex6 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        impar cadastro = new impar();
+        cadastro.entrada();
+        cadastro.somai();
+        cadastro.imprimir();
+    }
+    
+}
