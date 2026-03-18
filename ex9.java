95e9e8b000b8421ff4464e324607c27953386433
Thalyson_Gama_gc <thalysoncosta567@gmail.com>
2026-03-18 17:30:25 -0300

    Create ex9.java

---
+++ ex9.java
@@ -0,0 +1,12 @@
+package ex9;
+
+public class Ex9 {
+
+    public static void main(String[] args) {
+        Primo cadastro = new Primo();
+
+        cadastro.entrada();
+        cadastro.calcular();
+        cadastro.imprimir();
+    }
+}
