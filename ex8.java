0bf05178ea14d4e8bd8aa7cbc359ebd0fd5639ae
Thalyson_Gama_gc <thalysoncosta567@gmail.com>
2026-03-18 16:28:35 -0300

    Create ex8.java

---
+++ ex8.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ex8;
+
+/**
+ *
+ * @author thaly
+ */
+public class Ex8 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        fat cadastro = new fat();
+        cadastro.entrada();
+        cadastro.calcular();
+        cadastro.imprimir();
+    }
+    
+}
