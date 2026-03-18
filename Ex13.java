26dece5e45f33edbcf938054eafce7a624cff7f4
Thalyson_Gama_gc <thalysoncosta567@gmail.com>
2026-03-18 17:53:56 -0300

    Add Principal class with main method

---
+++ Ex13.java
@@ -0,0 +1,10 @@
+public class Principal {
+
+    public static void main(String[] args) {
+        ListaNumeros lista = new ListaNumeros();
+
+        lista.lerDados();
+        lista.mostrarPares();
+        lista.mostrarImpares();
+    }
+}
