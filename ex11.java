35d7aa3231b4e861e2eb334118b587f13b01ad82
Thalyson_Gama_gc <thalysoncosta567@gmail.com>
2026-03-18 17:42:44 -0300

    Add Ex11 class with main method for Pprimo

---
+++ ex11.java
@@ -0,0 +1,12 @@
+package ex11;
+
+public class Ex11 {
+
+    public static void main(String[] args) {
+        Pprimo cadastro = new Pprimo();
+
+        cadastro.entrada();
+        cadastro.calcular();
+        cadastro.imprimir();
+    }
+}
