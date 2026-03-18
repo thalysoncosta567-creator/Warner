ea362d88130421cce989cfa8a4dc8aa7c932a1c0
Thalyson_Gama_gc <thalysoncosta567@gmail.com>
2026-03-18 17:55:15 -0300

    Add main class to execute Numero methods

---
+++ ex14.java
@@ -0,0 +1,8 @@
+public class Main {
+	public static void main(String[] args) {
+		Numero novoNumero= new Numero();
+        novoNumero.entrada();
+        novoNumero.calculo();
+        novoNumero.imprimir();
+	}
+}
