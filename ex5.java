45113f639b1f65307be9791218eb38ed109e24f4
Thalyson_Gama_gc <thalysoncosta567@gmail.com>
2026-03-18 15:25:55 -0300

    Add Ex5 class with main method and cadastro usage

---
+++ ex5.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ex5;
+
+/**
+ *
+ * @author thaly
+ */
+public class Ex5 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       
+        par cadastro = new par();
+        cadastro.entrada();
+        cadastro.somap();
+        cadastro.imprimir();
+    }
+    
+}
