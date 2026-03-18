363a0c1682a6c14222108865abbbebf67c268de2
Thalyson_Gama_gc <thalysoncosta567@gmail.com>
2026-03-18 14:35:04 -0300

    Add Ex3 class with main method for cadastro

---
+++ ex3.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ex3;
+
+/**
+ *
+ * @author aluno9
+ */
+public class Ex3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        sexo cadastro = new sexo();
+        cadastro.entrada();
+        cadastro.imprimir();
+    }
+    
+}
